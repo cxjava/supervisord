c71428b11b346bbc35b04b81527b7165b8e5806a
Char <cxjava@users.noreply.github.com>
2019-05-29 17:45:29 +0800

    Update upx.sh

--- upx.sh
+++ upx.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find ./dist -xdev -maxdepth 3 -type f -name 'supervisord' -o -name 'supervisord.exe' | xargs ./upx
+find ./dist -xdev -maxdepth 3 -type f -name 'supervisord*' | xargs ./upx
